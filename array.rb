18967f6b11bbf9082d49e4699e4d7fd525f2535b
marcioaleson <marcioaleson@gmail.com>
2016-06-07 10:39:10 -0300

    Simples exemplos.

--- array.rb
+++ array.rb
@@ -4,4 +4,9 @@ puts nomes
 pessoa = {:nome=>"Marcio",:email=>"marcioaleson@gmail.com"}
 puts pessoa[:nome]
 puts pessoa[:email]
-
+numeros = [1,2,3,4,5]
+numeros.each {|numero| puts "O array tem o número: "+numero.to_s} #Interadores.
+puts numeros[0..1]
+puts numeros.first
+puts numeros.last
+puts numeros.take(2)
---
+++ hash.rb
@@ -0,0 +1,8 @@
+hash1 = {:john=>"Guitarra e voz", :paul=>"Baixo e voz",:george=>"Guitarra",:ring=>"bateria"}
+puts hash1
+puts hash1[:paul]
+hash2 = {"Fixnum"=>1,:Float=>1.23,1=>"um"}
+puts hash2
+puts hash2["Fixnum"]
+puts hash2[:Float]
+puts hash2[1]
