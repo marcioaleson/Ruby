44e1aa42b82b0760ba89cbde0c04f7a17b378551
marcioaleson <marcioaleson@gmail.com>
2016-08-23 12:40:37 -0300

    Alias de métodos em ruby.

---
+++ aliases.rb
@@ -0,0 +1,43 @@
+# encoding: utf-8
+class Carro 
+    
+    attr_reader :marca, :modelo, :tanque
+    attr_accessor :cor 
+    
+	def initialize(marca, modelo, cor, tanque)
+		@marca = marca
+		@modelo = modelo
+		@cor = cor
+		@tanque = tanque
+	end
+
+	def to_s
+       "Marca: #{@marca}, Modelo: #{@modelo}, Cor: #{@cor}, Tanque: #{@tanque}."
+	end
+	
+end
+
+class Carro
+
+    alias_method :to_s_old, :to_s
+    def to_s
+        "Esse é um jeito de mostrar isso: #{to_s_old}"
+    end
+
+end
+
+carro = Carro.new(:chevrolet, :corsa, :preto, 50)
+puts carro
+puts carro.to_s_old
+
+#Inserindo métodos.
+corsa = Carro.new(:chevrolet, :corsa, :preto, 40)
+class << corsa
+
+    def novo_metodo
+    	puts "Novo método."
+    end
+end
+
+puts corsa.novo_metodo
+puts carro.novo_metodo
