6f4e5541491a3359cf798a4528daf02adb055619
marcioaleson <marcioaleson@gmail.com>
2016-12-07 22:06:56 -0300

    Threads example synchronize.

--- threads/threads7.rb
+++ threads/threads7.rb
@@ -17,12 +17,27 @@ class Threads7
 		
 end
 
+class Threads7_2
+    attr_reader :valor
+
+
+	def initialize
+		@valor = 0
+	end
+
+	def incremente
+		@valor = valor + 1
+	end
+	
+	
+end
+
 t1 = Threads7.new
-t2 = Threads7.new
+t2 = Threads7_2.new
 t2.extend(MonitorMixin)
 
-t3 = Thread.new {100_000.times { t1.synchronize{t1.incremente} }}
-t4 = Thread.new {100_000.times {t2.synchronize{t2.incremente}}}
+t3 = Thread.new {100_000.times { t2.synchronize { t2.incremente }}}
+t4 = Thread.new {100_000.times { t2.synchronize { t2.incremente }}}
 
 t3.join
 t4.join
