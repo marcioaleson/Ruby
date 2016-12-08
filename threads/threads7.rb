ef0e7d49ace88d30715c1ad32637c62219bb0192
marcioaleson <marcioaleson@gmail.com>
2016-12-07 22:00:26 -0300

    Theads example sychronize.

---
+++ threads/threads7.rb
@@ -0,0 +1,30 @@
+require "monitor"
+
+class Threads7
+    attr_reader :valor
+    include MonitorMixin
+    
+    def initialize
+    	@valor = 0
+    	super
+    end
+
+    def incremente
+    	synchronize do 
+    		@valor = valor +1
+    	end
+    end  
+		
+end
+
+t1 = Threads7.new
+t2 = Threads7.new
+t2.extend(MonitorMixin)
+
+t3 = Thread.new {100_000.times { t1.synchronize{t1.incremente} }}
+t4 = Thread.new {100_000.times {t2.synchronize{t2.incremente}}}
+
+t3.join
+t4.join
+
+puts t2.valor
