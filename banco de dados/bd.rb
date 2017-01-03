ba8588374dc55d446ed4c4ff3443afa2dcdc8911
marcioaleson <marcioaleson@gmail.com>
2017-01-02 22:33:40 -0300

    Banco de dados com mysql2 gem.

--- banco de dados/bd.rb
+++ banco de dados/bd.rb
@@ -2,7 +2,7 @@ require "rubygems"
 require "sequel"
 require "mysql"
 
-Sequel.mysql(:user=>"root", :password=>"123456",:host=>"localhost", :database=>"alunos") do |con|
+Sequel.mysql(:user=>"root", :password=>"abcd1234",:host=>"localhost", :database=>"alunos") do |con|
     p con
 end
 
