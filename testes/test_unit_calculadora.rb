83115c4425085ade8680d5ffe5fa88d63bcc5c1f
marcioaleson <marcioaleson@gmail.com>
2017-02-13 22:44:59 -0300

    Outros tipos de assirts.

--- testes/test_unit_calculadora.rb
+++ testes/test_unit_calculadora.rb
@@ -6,6 +6,17 @@ class TesteCalculadora < Test::Unit::TestCase
     	@calculadora = Calculadora.new
     end
 
+    def test_objeto
+    	assert_kind_of(Calculadora, @calculadora)
+    	assert_match(/^\d$/, @calculadora.soma(1,1).to_s)
+    	assert_respond_to(@calculadora, :soma)
+    	assert_same(@calculadora, @calculadora)
+    end
+
+    def test_objetos
+    	assert_operator(@calculadora.soma(1,1), :>, @calculadora.soma(1,0))
+    end
+
     def test_adicao
     	assert_equal(2,@calculadora.soma(1,1), "1+1=2")
     end
