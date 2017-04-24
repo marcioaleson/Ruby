c2c97897b906480c8430e6ca4425658a94ceb2c7
marcioaleson <marcioaleson@gmail.com>
2017-04-24 20:49:22 -0300

    Ruby rdoc example.

---
+++ Rdoc/rdoc.rb
@@ -0,0 +1,9 @@
+# Essa é a classe base para todos os carros que vamos
+# criar no nosso programa. A partir dela criamos carros
+# de marcas específicas.
+# Autor: Márcio Áleson
+#Licença :: GPL
+
+class Carro
+
+end
