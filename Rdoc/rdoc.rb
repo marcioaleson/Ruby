f0755910f4477a8c8cf41dd1727888f861a94d4c
marcioaleson <marcioaleson@gmail.com>
2017-04-24 21:48:14 -0300

    Rdoc example.

--- Rdoc/rdoc.rb
+++ Rdoc/rdoc.rb
@@ -5,5 +5,25 @@
 #Licença :: GPL
 
 class Carro
+    attr_reader :marca, :modelo, :tanque
+    attr_accessor :cor
 
+    # Parãmetro obrigatório para criar o carro
+    # Não se esqueça de que todo carro vai ter os custos de:
+    # * IPVA
+    # * Seguro obrigatório
+    # * Seguro
+    # * Manutenção
+
+    def initialize(marca, modelo, cor, tanque)
+      @marca = marca
+      @modelo = modelo
+      @cor = cor
+      @tanque = tanque
+    end
+
+    # Converte  o carro e muam representação mais legível
+    def to_s
+      "Marca:#{@marca}, Modelo:#{@modelo}, Cor:#{@cor}, Tanque:#{@tanque}."
+    end
 end
